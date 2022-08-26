375c28a6fb7d0df1c59596055da3d2d6c2863c76
Richard Taujenis <Mozesthegreat@yahoo.com>
2022-08-26 17:52:18 +0200

    Added create tables in utils/index.js

--- server/utils/index.js
+++ server/utils/index.js
@@ -1,56 +1,17 @@
+
 const client = require("./connections");
+const tables = require('../users/queries');
 
-const execute = async (query) => {
-    try {
-        await client.connect();     // gets connection
-        await client.query(query);  // sends queries
-        return true;
-    } catch (error) {
-        console.error(error.stack);
-        return false;
-    } finally {
-        await client.end();         // closes connection
-    }
-};
-
-const users = `
-    CREATE TABLE IF NOT EXISTS users (
-	user_id serial PRIMARY KEY,
-	username VARCHAR ( 50 ) UNIQUE NOT NULL,
-	password VARCHAR ( 50 ) NOT NULL,
-	email VARCHAR ( 255 ) UNIQUE NOT NULL,
-
-    );`;
-
-const budget = `
-    CREATE TABLE IF NOT EXISTS account_roles (
-  user_id INT NOT NULL,
-  role_id INT NOT NULL,
-  grant_date TIMESTAMP,
-  PRIMARY KEY (user_id, role_id),
-  FOREIGN KEY (role_id)
-      REFERENCES roles (role_id),
-  FOREIGN KEY (user_id)
-      REFERENCES accounts (user_id)
-);
-    );`;
-
-const associations = `
-    CREATE TABLE account_roles (
-  user_id INT NOT NULL,
-  role_id INT NOT NULL,
-  grant_date TIMESTAMP,
-  PRIMARY KEY (user_id, role_id),
-  FOREIGN KEY (role_id)
-      REFERENCES roles (role_id),
-  FOREIGN KEY (user_id)
-      REFERENCES accounts (user_id)
-);
-    );`;
-
-
-execute(text).then(result => {
-    if (result) {
-        console.log('Table created');
-    }
-});
+client.query(tables.queryUsers, (err, res) => {
+    console.log("User table created");
+    console.log(err, res);
+})
+client.query(tables.queryBudget, (err, res) => {
+    console.log("Budget table created");
+    console.log(err, res);
+})
+client.query(tables.queryRelationship, (err, res) => {
+    console.log("Relationship table created");
+    console.log(err, res);
+    client.end();
+})
