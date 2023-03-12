ffb6585bcc961c4d0329f1ae4ac861f8f99f7ded
R Taujenis <Mozesthegreat@yahoo.com>
2023-03-12 22:25:47 +0100

    Added OverviewTable

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -2,47 +2,37 @@ import Navbar from '../Navbar';
 import { Navigate } from "react-router-dom"
 import React from "react"
 import { useState, useEffect } from "react";
-import { emailStore } from "../../store/store";
-import { idStore } from "../../store/store";
-import axios from 'axios';
-import Balance from '../Balance'
+import { emailStore, idStore } from "../../store/store";
 import OverviewTable from '../tables/OverviewTable';
+import Balance from '../../components/Balance'
+import  UserBalance  from '../../hooks/userBalance'
 
 
 const Dashboard = () => {
   const [transactions, setTransactions] = useState([]);
+  const [userId, setUserId] = useState(idStore.getState());
   const [balance, setBalance] = useState(0);
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
   const email = emailStore.getState();
 
-
-  const fetchData = async () => {
-    try {
-      const emailUser = JSON.stringify({
-        "email": email
-      });
-      const config = {
-        method: 'post',
-        url: 'http://localhost:5000/api/v1/email',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        data: emailUser
-      };
-      const response = await axios(config);
-      const userId = response.data;
-      idStore.setState({id: userId});
-      const transactionsResponse = await axios(`http://localhost:5000/api/v1/expense/${userId}`);
-      setTransactions(transactionsResponse.data);
-      setLoading(true);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
   useEffect(() => {
+    const fetchData = async () => {
+      console.log("in  fetchData")
+      const data = await UserBalance(email);
+      setTransactions(data);
+      console.log(transactions);
+  
+      if (data.length > 0) {
+        console.log("in  fetchData set")
+        console.log(data);
+        const newBalance = data[0].balance.toFixed(2);
+        setBalance(newBalance);
+      }
+    };
+  
+    console.log("in  useEffect")
     fetchData();
-  }, []);
+  }, [email, loading]);
 
   if (email === null) {
     return <Navigate replace to="/" />;
@@ -51,10 +41,10 @@ const Dashboard = () => {
   return (
     <div className="row d-flex justify-content-center">
       <Navbar className="d-grid gap-5 me-5" />
-      {loading ? (
-        <div>
+      {!loading ? (
+        <div className="d-flex justify-content-between">
           <Balance balance={balance} />
-          <OverviewTable transaction={transactions}/>
+          <OverviewTable transactions={transactions}/>
         </div>
       ) : (
         <div className="d-flex justify-content-center align-items-center vh-100">
@@ -66,3 +56,4 @@ const Dashboard = () => {
 };
 
 export default Dashboard;
+
